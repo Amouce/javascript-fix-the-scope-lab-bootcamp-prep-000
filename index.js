922edf7cafa52b571785893e0202767ccb3fb84b
adaptable-tutorial-4108 <danielamouce@gmail.com>
2019-06-23 04:16:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,15 +15,26 @@ function add2(n) {
 
 }
 
+<<<<<<< HEAD
 var funkyFunction = function () {
   return function () {
     return 
     }
+=======
+var funkyFunction = function funkyFunction() {
+  return function() {
+    return "FUNKY!"
+  }
+>>>>>>> bfd43a31ff04c80a091a99054af45b63eccef49b
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
+<<<<<<< HEAD
 var theFunk = function () {
   return 'FUNKY!'
     
 }
+=======
+var theFunk = funkyFunction()
+>>>>>>> bfd43a31ff04c80a091a99054af45b63eccef49b
